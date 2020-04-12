88ef7785dc245f6c79dabddb95cadc94aadfd3a5
michaelramirez <michaelramirez@iheartmedia.com>
2020-04-12 14:03:15 -0500

    Fix firebase project id for prod build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: 'AIzaSyAICxg_Gz0_ip1M-30QNK_pftfNQg7z-pI',
     authDomain: 'online-poker-c86b2.firebaseapp.com',
     databaseURL: 'https://online-poker-c86b2.firebaseio.com',
-    projectId: 'online-poker-c86b2"',
+    projectId: 'online-poker-c86b2',
     storageBucket: 'online-poker-c86b2.appspot.com',
     messagingSenderId: '291010386105',
     appId: '1:291010386105:web:2b119b50867f1a6228a368',
