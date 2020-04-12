d5a25ae8176157c7233c664fa974bb1767e70b21
michaelramirez <michaelramirez@iheartmedia.com>
2020-04-12 17:58:51 -0500

    Create Game History component.

--- src/app/components/poker-table/poker-table.module.ts
+++ src/app/components/poker-table/poker-table.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokerTableComponent } from './poker-table.component';
 import {MatButtonModule, MatProgressBarModule} from '@angular/material';
+import {GameHistoryModule} from '../game-history/game-history.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatProgressBarModule,
-    MatButtonModule
-  ],
+    imports: [
+        CommonModule,
+        MatProgressBarModule,
+        MatButtonModule,
+        GameHistoryModule
+    ],
   declarations: [
     PokerTableComponent
   ],
